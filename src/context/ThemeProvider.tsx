4b5364a09985ba5e7496efd9c9b705a8976148b9
Shariq Choudhary <khanshariq92213@gmail.com>
2025-04-18 22:26:14 +0530

    Minor Change

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,8 +1,18 @@
 "use client"
+
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
+import { useEffect } from "react"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
+  // Force dark mode by default
+  useEffect(() => {
+    document.documentElement.classList.add("dark")
+  }, [])
 
+  return (
+    <NextThemesProvider {...props} defaultTheme="dark" forcedTheme="dark">
+      {children}
+    </NextThemesProvider>
+  )
+}
