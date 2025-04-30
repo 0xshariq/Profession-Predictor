f8203291d2d9c2f5ead5aaa053158793eb0fd3a3
Shariq Choudhary <khanshariq92213@gmail.com>
2025-04-30 10:01:36 +0530

    Minor change

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -145,13 +145,13 @@ const SignIn = () => {
                           className="pr-10"
                           {...field}
                         />
-                        <button
+                        <Button
                           type="button"
                           onClick={() => setShowPassword(!showPassword)}
                           className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-foreground"
                         >
                           {showPassword ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
-                        </button>
+                        </Button>
                       </div>
                     </FormControl>
                     <FormMessage />
